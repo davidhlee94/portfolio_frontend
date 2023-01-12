11d9d822db8c613dcc5aacab072aa9c6f3651a26
davidhlee94 <davidhlee94@gmail.com>
2023-01-12 08:59:17 -0800

    updated resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -21,7 +21,7 @@ const Resume = () => {
         </a>
       </div>
       <iframe
-        src="https://drive.google.com/file/d/1sYvZGvvis3dYTG9ZZl_VWA39esCnhGxc/preview"
+        src="https://drive.google.com/file/d/196VvCPR-irbj3yQmBgufTNcbKnBiCGr5/preview"
         allow="autoplay"
         className="iframe"
       ></iframe>
