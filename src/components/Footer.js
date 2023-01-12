5cbefba2c52c0566b3c470a2ec5e23758c820d41
davidhlee94 <davidhlee94@gmail.com>
2023-01-11 17:44:58 -0800

    fixed link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <a href="https://www.linkedin.com/in/iamdavidhanlee/">
           <img className="icon" src="./images/linkedin.png" />
         </a>
-        <a href="https://www.linkedin.com/in/iamdavidhanlee/">
+        <a href="https://github.com/davidhlee94">
           <img className="icon" src="./images/github.png" />
         </a>
         <a href="mailto:myemailaddress@gmail.com">
