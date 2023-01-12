8c3b49b1331429c37cac02528ee51170c49db138
davidhlee94 <davidhlee94@gmail.com>
2023-01-11 17:42:46 -0800

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,20 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer-container">
-      <img src="%PUBLIC_URL%/linkedin.png" />
+      <div className="icon-container">
+        <a href="https://www.linkedin.com/in/iamdavidhanlee/">
+          <img className="icon" src="./images/linkedin.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/iamdavidhanlee/">
+          <img className="icon" src="./images/github.png" />
+        </a>
+        <a href="mailto:myemailaddress@gmail.com">
+          <img className="icon" src="./images/email.png" />
+        </a>
+      </div>
+      <div>
+        <h6 className="name">©DAVID LEE</h6>
+      </div>
     </div>
   );
 }
